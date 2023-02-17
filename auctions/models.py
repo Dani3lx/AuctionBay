f95fe15e06bb3a889a3e04b4d924638bb6592845
Dani3lx <1danielxu@gmail.com>
2023-02-17 00:04:42 -0500

    Added all the models for the database

--- auctions/models.py
+++ auctions/models.py
@@ -5,18 +5,20 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+# Listing names are unique
 class Listing(models.Model):
-    listing_name = models.CharField(max_length=64)
+    listing_name = models.CharField(primary_key=True, max_length=64)
     listing_desc = models.CharField(max_length=256)
     starting_bid = models.IntegerField()
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
     post_date = models.TimeField(auto_now=False, auto_now_add=False)
 
 class Bid(models.Model):
-    origin = models.CharField(max_length=64)
-    destination = models.CharField(max_length=64)
-    duration = models.IntegerField()
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE)
+    value = models.IntegerField()
+    item = models.ForeignKey(Listing, on_delete=models.CASCADE)
 
 class Comment(models.Model):
-    origin = models.CharField(max_length=64)
-    destination = models.CharField(max_length=64)
-    duration = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=512)
+    item = models.ForeignKey(Listing, on_delete=models.CASCADE)
